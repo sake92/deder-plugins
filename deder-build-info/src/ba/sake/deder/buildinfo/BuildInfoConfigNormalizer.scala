586dbcd81929258b6d9d81cb0a14ac0590197df7
Sakib Hadziavdic <sakib@sake.ba>
2026-05-25 17:14:14 +0200

    feat: implement BuildInfo config normalizer with tests

---
+++ deder-build-info/src/ba/sake/deder/buildinfo/BuildInfoConfigNormalizer.scala
@@ -0,0 +1,68 @@
+package ba.sake.deder.buildinfo
+
+import ba.sake.deder.BuildInfo
+import scala.jdk.CollectionConverters.*
+
+final case class ResolvedBuildInfoConfig(
+    enabled: Boolean,
+    packageName: Option[String],
+    objectName: String,
+    includeGitHash: Boolean,
+    includeTimestamp: Boolean,
+    extra: Map[String, String],
+    scalaVersion: Option[String]
+)
+
+object BuildInfoConfigNormalizer {
+
+  def normalize(
+      moduleId: String,
+      moduleType: String,
+      scalaVersion: Option[String],
+      config: BuildInfo.BuildInfoPluginConfig
+  ): ResolvedBuildInfoConfig = {
+    val defaults = config.defaults
+    val overrideConfig = Option(config.modules.get(moduleId))
+
+    val enabled = overrideConfig
+      .flatMap(value => Option(value.enabled).map(_.booleanValue()))
+      .getOrElse(defaults.enabled)
+
+    val packageName = overrideConfig
+      .flatMap(value => Option(value.packageName))
+      .orElse(trimToOption(defaults.packageName))
+
+    val objectName = overrideConfig
+      .flatMap(value => trimToOption(value.objectName))
+      .getOrElse(defaults.objectName)
+
+    val includeGitHash = overrideConfig
+      .flatMap(value => Option(value.includeGitHash).map(_.booleanValue()))
+      .getOrElse(defaults.includeGitHash)
+
+    val includeTimestamp = overrideConfig
+      .flatMap(value => Option(value.includeTimestamp).map(_.booleanValue()))
+      .getOrElse(defaults.includeTimestamp)
+
+    val extra = overrideConfig
+      .flatMap(value => Option(value.extra).map(_.asScala.toMap))
+      .getOrElse(defaults.extra.asScala.toMap)
+      .view
+      .mapValues(_.trim)
+      .toMap
+      .filter { case (key, value) => key.trim.nonEmpty && value.nonEmpty }
+
+    ResolvedBuildInfoConfig(
+      enabled = enabled,
+      packageName = packageName,
+      objectName = objectName,
+      includeGitHash = includeGitHash,
+      includeTimestamp = includeTimestamp,
+      extra = extra,
+      scalaVersion = scalaVersion
+    )
+  }
+
+  private def trimToOption(value: String): Option[String] =
+    Option(value).map(_.trim).filter(_.nonEmpty)
+}
---
+++ deder-build-info/test/src/ba/sake/deder/buildinfo/BuildInfoConfigNormalizerSuite.scala
@@ -0,0 +1,149 @@
+package ba.sake.deder.buildinfo
+
+import ba.sake.deder.BuildInfo
+import munit.FunSuite
+
+import java.util.{List as JList, Map as JMap}
+
+class BuildInfoConfigNormalizerSuite extends FunSuite {
+
+  test("defaults resolve to expected values") {
+    val defaults = new BuildInfo.ModuleDefaults(
+      true,
+      null,
+      "BuildInfo",
+      false,
+      false,
+      JMap.of()
+    )
+    val config = new BuildInfo.BuildInfoPluginConfig(defaults, JMap.of())
+
+    val resolved = BuildInfoConfigNormalizer.normalize(
+      moduleId = "my-module",
+      moduleType = "JAVA",
+      scalaVersion = None,
+      config = config
+    )
+
+    assertEquals(resolved.enabled, true)
+    assertEquals(resolved.packageName, None)
+    assertEquals(resolved.objectName, "BuildInfo")
+    assertEquals(resolved.includeGitHash, false)
+    assertEquals(resolved.includeTimestamp, false)
+    assertEquals(resolved.extra, Map.empty)
+    assertEquals(resolved.scalaVersion, None)
+  }
+
+  test("packageName resolves to Some when set") {
+    val defaults = new BuildInfo.ModuleDefaults(
+      true,
+      "com.example",
+      "BuildInfo",
+      false,
+      false,
+      JMap.of()
+    )
+    val config = new BuildInfo.BuildInfoPluginConfig(defaults, JMap.of())
+
+    val resolved = BuildInfoConfigNormalizer.normalize(
+      moduleId = "my-module",
+      moduleType = "SCALA",
+      scalaVersion = Some("3.7.4"),
+      config = config
+    )
+
+    assertEquals(resolved.packageName, Some("com.example"))
+    assertEquals(resolved.scalaVersion, Some("3.7.4"))
+  }
+
+  test("module override wins over defaults") {
+    val defaults = new BuildInfo.ModuleDefaults(
+      true,
+      null,
+      "BuildInfo",
+      false,
+      false,
+      JMap.of()
+    )
+    val moduleOverride = new BuildInfo.ModuleOverride(
+      null,
+      "com.override",
+      "MyBuildInfo",
+      true,
+      null,
+      JMap.of("myKey", "myValue")
+    )
+    val config = new BuildInfo.BuildInfoPluginConfig(defaults, JMap.of("my-module", moduleOverride))
+
+    val resolved = BuildInfoConfigNormalizer.normalize(
+      moduleId = "my-module",
+      moduleType = "SCALA",
+      scalaVersion = Some("3.7.4"),
+      config = config
+    )
+
+    assertEquals(resolved.packageName, Some("com.override"))
+    assertEquals(resolved.objectName, "MyBuildInfo")
+    assertEquals(resolved.includeGitHash, true)
+    assertEquals(resolved.includeTimestamp, false) // not overridden, uses default
+    assertEquals(resolved.extra, Map("myKey" -> "myValue"))
+  }
+
+  test("disabled module resolves enabled=false") {
+    val defaults = new BuildInfo.ModuleDefaults(
+      true,
+      null,
+      "BuildInfo",
+      false,
+      false,
+      JMap.of()
+    )
+    val moduleOverride = new BuildInfo.ModuleOverride(
+      false,
+      null,
+      null,
+      null,
+      null,
+      null
+    )
+    val config = new BuildInfo.BuildInfoPluginConfig(defaults, JMap.of("my-module", moduleOverride))
+
+    val resolved = BuildInfoConfigNormalizer.normalize(
+      moduleId = "my-module",
+      moduleType = "SCALA",
+      scalaVersion = None,
+      config = config
+    )
+
+    assertEquals(resolved.enabled, false)
+  }
+
+  test("extra values merge from override and defaults") {
+    val defaults = new BuildInfo.ModuleDefaults(
+      true,
+      null,
+      "BuildInfo",
+      false,
+      false,
+      JMap.of("defaultKey", "defaultVal")
+    )
+    val moduleOverride = new BuildInfo.ModuleOverride(
+      null,
+      null,
+      null,
+      null,
+      null,
+      JMap.of("overrideKey", "overrideVal")
+    )
+    val config = new BuildInfo.BuildInfoPluginConfig(defaults, JMap.of("my-module", moduleOverride))
+
+    val resolved = BuildInfoConfigNormalizer.normalize(
+      moduleId = "my-module",
+      moduleType = "SCALA",
+      scalaVersion = None,
+      config = config
+    )
+
+    assertEquals(resolved.extra, Map("overrideKey" -> "overrideVal"))
+  }
+}
